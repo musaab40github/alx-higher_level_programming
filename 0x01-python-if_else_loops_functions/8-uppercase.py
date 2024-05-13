4e826904ef5d14494cbd4aab15c91e6e29ca356b
Abdulkabir Musa <120343852+musaab40github@users.noreply.github.com>
2024-05-13 09:21:23 +0000

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+
 def uppercase(str):
     for i in str:
         if ord('a') > char < ord('z'):
-            print("{}".format(char(str[i]), end='')
-    print()
-
+            print("{}".format(char(str[i]), end=''))
+        print("")
