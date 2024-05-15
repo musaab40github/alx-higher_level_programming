d4abcc39a7a8ec8beed5e1a74267923a6e40294c
Abdulkabir Musa <120343852+musaab40github@users.noreply.github.com>
2024-05-15 16:28:25 +0000

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,11 +4,7 @@ if __name__ = "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    if add:
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif sub:
-        print("{} - {} = {}".format(a, b, sub(a,b)))
-    elif mul:
-        print("{} * {} = {}".format(a, b, mul(a,b)))
-    else:
-        print("{} / {} = {}".format(a, b, div(a,b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a,b)))
+    print("{} * {} = {}".format(a, b, mul(a,b)))
+    print("{} / {} = {}".format(a, b, div(a,b)))
