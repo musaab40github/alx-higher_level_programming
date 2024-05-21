d85fcab41f8a63f06aab0c298d6c19598358f6a8
Abdulkabir Musa <120343852+musaab40github@users.noreply.github.com>
2024-05-21 15:09:17 +0000

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    new_list = []
     for x in my_list:
-        if my_list[x] == search:
-            my_list[x].append(replace)
-    return my_list
+        if x == search:
+            new_list.append(replace)
+        else:
+            new_list.append(x)
+    return new_list
