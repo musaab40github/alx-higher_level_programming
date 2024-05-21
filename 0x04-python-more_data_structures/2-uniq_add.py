74ea7bdd40802ebb5958891fb9496fc503b3fd42
Abdulkabir Musa <120343852+musaab40github@users.noreply.github.com>
2024-05-21 15:40:41 +0000

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    add = 0
-    for i in my_list:
-        add += my_list[i]
-        if my_list[i] == my_list:
-            continue
-        i++
-    return add
+    add = set(my_list)
+    result = sum(add)
+    return result
